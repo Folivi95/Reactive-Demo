135bf4a4146912cd2ba0ac9b0e697fe806365339
Joshua Folivi <joshua.folivi@gmail.com>
2019-09-09 17:49:17 +0100

    initial commit

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customer = new Customer();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save() {
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
+}
